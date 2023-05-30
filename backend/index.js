90682ec93a14e796e133c5c56f68bf6cf3741d26
yaniv <yaniv@DESKTOP-K7C29F9>
2023-05-30 17:14:30 +0300

    configured socket.io to server

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,28 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 const PORT = 3001;
 
 app.use(cors());
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id + " connected");
+
+  socket.on("disconnect", () => {
+    console.log(socket.id + " disconnected");
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
