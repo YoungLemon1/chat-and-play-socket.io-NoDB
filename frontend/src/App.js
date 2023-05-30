1d473509f1d4b2e32ec9d74f017dc399c906a56a
yaniv <yaniv@DESKTOP-K7C29F9>
2023-05-30 17:41:27 +0300

    established connection between client and server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
-  return <div className="App">asd</div>;
+  return <div className="App"></div>;
 }
 
 export default App;
