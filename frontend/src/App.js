82fb1490f15abaea5fb6ef78fb97ecef8dbc22b4
yaniv <yaniv@DESKTOP-K7C29F9>
2023-06-04 19:48:03 +0300

    css layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ const socket = io.connect("http://localhost:3001");
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
@@ -17,24 +18,28 @@ function App() {
 
   return (
     <div className="App">
-      <h3>Join a chat</h3>
-      <input
-        type="text"
-        placeholder="Name..."
-        onChange={(event) => {
-          setUsername(event.target.value);
-        }}
-      />
-      <input
-        type="text"
-        placeholder="Room ID..."
-        onChange={(event) => {
-          setRoom(event.target.value);
-        }}
-      />
-      <button onClick={joinRoom}>Join room</button>
-
-      <Chat socket={socket} username={username} room={room}></Chat>
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join a chat</h3>
+          <input
+            type="text"
+            placeholder="Name..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room}></Chat>
+      )}
     </div>
   );
 }
