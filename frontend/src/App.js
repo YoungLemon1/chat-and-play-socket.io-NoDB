581eb4810d486690b3fc4c6a82bdc86d79823f7a
yaniv <yaniv@DESKTOP-K7C29F9>
2023-06-04 17:41:06 +0300

    enter room functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import io from "socket.io-client";
+import Chat from "./components/Chat";
 
 const socket = io.connect("http://localhost:3001");
 
@@ -24,7 +25,16 @@ function App() {
           setUsername(event.target.value);
         }}
       />
+      <input
+        type="text"
+        placeholder="Room ID..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
       <button onClick={joinRoom}>Join room</button>
+
+      <Chat socket={socket}></Chat>
     </div>
   );
 }
